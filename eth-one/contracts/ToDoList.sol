302b7095342a82423e9a89ccdb87f7485a1ab3e4
Yash Pandya <ypandya.136@gmail.com>
2022-06-28 13:01:34 +0530

    add tasks

--- eth-one/contracts/ToDoList.sol
+++ eth-one/contracts/ToDoList.sol
@@ -1,11 +1,23 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    
     // it will take initial value as 0
-    uint public myNum;
+    uint256 public taskCount;
 
-    struct Task{
-t788truyfuygfg
+    struct Task {
+        uint256 id;
+        string text;    
+        bool completed;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("Created first ever web3.0 Application");
+    }
+
+    function createTask(string memory _text) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _text, false);
     }
 }
